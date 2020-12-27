672529fbc2536c103e8d0174b4c9d70ec1b6ba60
Jeremie David <jeremiedavid9@hotmail.com>
2020-12-27 17:10:00 -0500

    Added new Users view to show all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
     before_action :logged_in_user, only: [:show]
+
+    def index
+      @user = User.all
+    end
+
     def show
       @user = User.find(params[:id])
     end
