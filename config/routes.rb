21d8bcfaa327dbc2599aba44683f7222ef464390
Jeremie David <jeremiedavid9@hotmail.com>
2020-12-30 20:56:45 -0500

    Fixed route for Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'images#index'
   get '/signup', to: 'users#new'
-  resources :images
-  get '/users', to: 'users#index'
+  resources :images, :users
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
